f78449084278ee0b7f560e07f5b8812e5aab25d5
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-05-03 17:02:44 +0300

    Commit

--- src/Components/LandingPage/Main/MainP2/MainP2.jsx
+++ src/Components/LandingPage/Main/MainP2/MainP2.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import data2 from "./MainP2D.js"
 
 const MainP2 = () => {
     return (
         <section>
             <h1>Hi</h1>
+            {data2.map(item => {
+                return (
+                    <img src={item.nft}/>
+                )
+            })}
         </section>
     );
 };
