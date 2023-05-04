d190fd341cedc157ebae2985226c91abd497b1dc
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-05-04 17:02:14 +0300

    Commit

--- src/Components/LandingPage/Main/MainP2/MainP2.jsx
+++ src/Components/LandingPage/Main/MainP2/MainP2.jsx
@@ -20,22 +20,68 @@ const MainP2 = () => {
           Top Collections
         </h2>
       </div>
-      <div className=" py-5 my-7 child:mx-2 child:border child:py-4 child:rounded-full font-poppi grid grid-flow-col justify-stretch">
-        <button onClick={() => filterMenu(artData)}>Art</button>
-        <button onClick={() => filterMenu(collData)}>Collectibles</button>
-        <button onClick={() => filterMenu(spaceData)}>Space</button>
-        <button onClick={() => filterMenu(landData)}>Virtual Worlds</button>
-        <button onClick={() => filterMenu(sportData)}>Sports</button>
-        <button onClick={() => filterMenu(moreData)}>More</button>
+      <div className=" py-5 my-7 transition-all child:transition-all child:font-semibold child:text-xl child:mx-2 child:border-2 child:py-4 child:rounded-full font-poppi grid grid-flow-col justify-stretch">
+        <button
+          onClick={() => filterMenu(artData)}
+          className={`  bg-${
+            item == artData ? "[#2F80ED]" : "transparent"
+          } hover:bg-[#113a61]`}
+        >
+          Art
+        </button>
+        <button
+          onClick={() => filterMenu(collData)}
+          className={` bg-${
+            item == collData ? "[#2F80ED]" : "transparent"
+          } hover:bg-[#113a61]`}
+        >
+          Collectibles
+        </button>
+        <button
+          onClick={() => filterMenu(spaceData)}
+          className={`  bg-${
+            item == spaceData ? "[#2F80ED]" : "transparent"
+          } hover:bg-[#113a61]`}
+        >
+          Space
+        </button>
+        <button
+          onClick={() => filterMenu(landData)}
+          className={`  bg-${
+            item == landData ? "[#2F80ED]" : "transparent"
+          } hover:bg-[#113a61]`}
+        >
+          Virtual Worlds
+        </button>
+        <button
+          onClick={() => filterMenu(sportData)}
+          className={`  bg-${
+            item == sportData ? "[#2F80ED]" : "transparent"
+          } hover:bg-[#113a61]`}
+        >
+          Sports
+        </button>
+        <button
+          onClick={() => filterMenu(moreData)}
+          className={`  bg-${
+            item == moreData ? "[#2F80ED]" : "transparent"
+          } hover:bg-[#113a61]`}
+        >
+          More
+        </button>
       </div>
-      <div className=" grid grid-cols-4 gap-x-7 gap-y-5 grid-rows-2 w-full h-fit">
+      <div className=" grid grid-cols-4 gap-x-7 gap-y-5 grid-rows-1 w-full min-h-min">
         {item.map((item) => {
           return (
             <div key={item.id}>
               <div className=" relative">
                 {/* <div className=" -inset-2 z-30 absolute bg-gradient-to-br from-[#8b8b8b] transition-all rounded-xl"></div> */}
-                <div className=" bg-gradient-to-tl from-[#2F2960] to-[#2F2960] relative  z-50">
-                  {/* <img src={item.nft} alt={item.NFTalt} /> */}
+                <div className="h-[35rem] bg-gradient-to-tl from-[#352868] flex flex-col items-center  to-[#2F2960] relative z-50 rounded-xl child:object-cover object-cover">
+                  <img
+                    src={item.nft}
+                    alt={item.NFTalt}
+                    className=" w-11/12 object-cover rounded-xl h-80 my-3"
+                  />
                   <h4>{item.OwnerName}</h4>
                   <h5>{item.OwnerSorName}</h5>
                   <div>
