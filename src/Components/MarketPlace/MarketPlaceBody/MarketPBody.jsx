25110d4f50b5cb6d96c45140af25c56249e403d9
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-05-15 11:47:30 +0300

    Commit

--- src/Components/MarketPlace/MarketPlaceBody/MarketPBody.jsx
+++ src/Components/MarketPlace/MarketPlaceBody/MarketPBody.jsx
@@ -9,13 +9,12 @@ const MarketPBody = () => {
   console.log(darkLight);
   return (
     <article
-      className={` flex flex-col items-center h-full overflow-x-hidden text-center`}
+      className={` flex flex-col items-center w-[92%] ml-auto h-full overflow-x-hidden text-center`}
     >
-      <section className=" relative mt-28 w-[92%] ml-auto flex justify-between items-center gap-x-10   mb-5">
+      <section className=" relative mt-28 w-full ml-auto flex flex-col justify-center sss:flex-row sss:justify-between items-center sss:gap-x-10 mb-5">
         <div
           className=" [&>h1]:z-20 [&>h1]:relative [&>p]:z-20 [&>p]:relative
-                         w-full h-72 relative 
-                            text-white 
+                         ml-5 w-11/12 sss:w-96 lg:w-[80%] slg:ml-0 slg:w-full h-72 relative text-white
                          "
         >
           <img
@@ -23,74 +22,82 @@ const MarketPBody = () => {
             className=" absolute z-10 top-2 w-[53rem] h-[18rem] object-cover rounded-3xl"
             alt="Marketplace"
           />
-          <h1 className=" relative text-4xl ml-8 mt-10 font-headerFont font-semibold text-left">
+          <h1 className=" relative text-2xl slg:text-4xl ml-8 mt-10 font-headerFont font-semibold text-left">
             Discover, Collect, Sell
             <br /> and Create your NFT
           </h1>
-          <p className=" text-left ml-8  text-[#B7A7F9] font-poppi my-6">
+          <p className=" text-left ml-8 text-sm mr-20 sss:mr-1 text-[#B7A7F9] font-poppi my-6">
             Digital marketplace for crypto collectibles and non fungible tokens
           </p>
-          <div className=" z-20 relative font-semibold font-poppi text-left mt-12 ml-8">
-            <button className=" mr-5 bg-[#6F4FF2] px-10 py-3 rounded-lg">
+          <div className=" z-20 relative font-semibold child:transition-all font-poppi text-left mt-12 ml-8">
+            <button className=" hover:bg-[#6a49f2] hover:text-slate-200 mr-5 bg-[#6F4FF2] px-6 py-2 slg:px-10 slg:py-3 rounded-lg">
               Explore
             </button>
-            <button className=" bg-[#DC3546] px-10 py-3 rounded-lg">
+            <button className=" hover:bg-[#cc3343] hover:text-slate-200 bg-[#DC3546] px-6 py-2 slg:px-10 slg:py-3 rounded-lg">
               Create
             </button>
           </div>
         </div>
 
         <div
-          className={`w-[90%] mr-16 h-full mt-4 rounded-[23px] flex justify-between items-center  ${
+          className={` ml-5 w-11/12 mt-20 sss:w-96 lg:w-[80%] slg:ml-0 slg:w-full sss:mr-16 h-full sss:mt-4 rounded-[23px] flex flex-col sss:flex-row justify-between items-center ${
             darkLight.darkMode
               ? "bg-[#1D1932] text-white"
               : "bg-[#FFFFFF] text-black"
           }`}
         >
-          <div>
+          <div className=" mt-4 sss:mt-0 flex justify-center sss:block">
             <img
               src={redNft}
-              alt="fluid nft w-96"
+              alt="fluid nft "
               className=" rounded-[23px] 
-                ml-4 h-60 object-cover"
+                ml-4 w-10/12 h-5/12 xlsm:w-8/12   sss:w-48 sss:h-60 lsmd:w-60 object-cover"
             />
           </div>
-          <div className=" w-2/3 h-60 ml-9  mr-3 flex flex-col items-start">
+          <div className=" w-2/3 h-fit ml-7 imd:ml-9  mr-3 flex flex-col items-start">
             <div className=" flex items-center  ">
               <img
                 src={verfiedUser}
                 alt="user logo"
-                className="rounded-full mr-4"
+                className="rounded-full w-8 imd:w-12 dxl:w-fit mr-2 imd:mr-4"
               />
-              <h2 className=" font-medium font-headerFont">
-                user#{Math.floor(Math.random() * 1000)}{" "}
+              <h2 className="text-sm imd:text-lg font-medium font-headerFont">
+                user#999
               </h2>
               <span className="relative flex h-3 w-3 ml-2">
                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
                 <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
               </span>
             </div>
-            <h3 className=" text-2xl font-semibold mt-3 font-poppi">
+            <h3 className="text-sm imd:text-md dxl:text-2xl font-semibold mt-3 font-poppi">
               Birghten LQ
             </h3>
 
-            <div className="flex  justify-between w-full font-poppi">
-              <div>
+            <div className="flex justify-between dxl:relative top-3 mt-2 sss:mt-0 dxl:flex-row sss:flex-col flex-row  w-full font-poppi">
+              <div className="ddxl:text-md imd:text-sm text-xs">
                 <h5>Auction time</h5>
-                <h5 className=" text-[#6C7AA0] text-left mt-1">3h 1m 50s</h5>
+                <h5 className=" text-[#6C7AA0] text-center my-2 dxl:my-0 dxl:text-left mt-1">
+                  3h 1m 50s
+                </h5>
               </div>
-              <div>
+              <div className="ddxl:text-md imd:text-sm text-xs">
                 <h5>
                   Current Bid :
                   <span className=" text-[#6F4FF2]"> 0.05 ETH</span>
                 </h5>
-                <h5 className=" text-[#6C7AA0] text-right">0.15 ETH</h5>
+                <h5 className=" text-[#6C7AA0] text-center my-2 dxl:my-0 dxl:text-right">
+                  0.15 ETH
+                </h5>
               </div>
             </div>
 
-            <div className="w-full child:w-1/2 mt-8 child:py-3 gap-x-8 child:font-medium child:rounded-lg flex justify-stretch">
-              <button className="bg-[#6F4FF2] ">Place a Bid</button>
-              <button className="bg-[#DC3546]">Details</button>
+            <div className="w-full child:w-1/2 mt-2 text-white text-xs slg:text-sm dxl:text-md dxl:mt-8 child:py-2 slg:child:py-3 gap-x-8 child:transition-all child:font-medium child:rounded-lg flex justify-stretch mb-4 sss:mb-0">
+              <button className="hover:bg-[#6a49f2] hover:text-slate-200 bg-[#6F4FF2] ">
+                Bid
+              </button>
+              <button className=" hover:bg-[#cc3343] hover:text-slate-200 bg-[#DC3546]">
+                Details
+              </button>
             </div>
           </div>
         </div>
