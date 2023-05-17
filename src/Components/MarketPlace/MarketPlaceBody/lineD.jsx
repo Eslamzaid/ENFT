45fb8163af525903787169afdf3bfeaa3d237a08
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-05-17 11:37:57 +0300

    Chart_Changed

--- src/Components/MarketPlace/MarketPlaceBody/lineD.jsx
+++ src/Components/MarketPlace/MarketPlaceBody/lineD.jsx
@@ -1,65 +1,81 @@
-import { useContext } from "react";
-import { DarkLightContext } from "../MarketP";
+import React, { PureComponent } from "react";
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
   Tooltip,
-  Filler,
   Legend,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
-import { faker } from "@faker-js/faker";
-// import
+  ResponsiveContainer,
+} from "recharts";
 
-//#262049
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Filler,
-  Legend
-);
-
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top",
-    },
-    title: {
-      display: true,
-      text: "ETH Price",
-    },
+const data = [
+  {
+    name: "Page A",
+    pv: 2400,
+    amt: 2400,
   },
-};
+  {
+    name: "Page B",
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
-export function lineData() {
-  const darkLight = useContext(DarkLightContext);
-  const data = {
-    labels,
-    datasets: [
-      {
-        fill: true,
-        label: "Price",
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        borderColor: "rgb(108,81,220)",
-        // backgroundColor: "rgb(37,30,71)",
-        backgroundColor: `${
-          darkLight.darkMode ? "rgb(37,30,71)":"rgb(240,237,254)"
-        }`,
-      },
-    ],
-  };
-  return <Line options={options} data={data} />;
+export default class lineData extends PureComponent {
+  render() {
+    return (
+      <ResponsiveContainer width="95%" height="95%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid stroke="#8884d8" strokeDasharray="3 3" />
+          <XAxis dataKey="name" stroke="#8884d8" />
+          <YAxis stroke="#8884d8" />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="pv"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    );
+  }
 }
-
-export default lineData;
